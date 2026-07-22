8125e60f76e02d4c8091d15eec5b7c4e2cea1e69
ochisrj <kittiphumisarayci57@gmail.com>
2026-07-22 18:12:29 +0700

    ada

---
+++ homework/Lab2/Test04_tax.java
@@ -0,0 +1,18 @@
+package Lab2;
+import java.util.Scanner;
+
+public class Test04_tax {   
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        double price,vat,total;
+       
+        System.out.print("Input price : ");
+        price = kb.nextDouble();
+        vat = price *7 / 100;
+        total = price + vat;
+        System.out.println("=== Calculate VAT ===");
+        System.out.printf("Total (price + vat) : %.2f" , total);
+        kb.close();
+    }
+    
+}
