5f0e97d6435aff20ecbfac857fd2a5324d0de59f
ochisrj <kittiphumisarayci57@gmail.com>
2026-07-22 17:15:28 +0700

    week 2

---
+++ homework/Lab2/fruitprice.java
@@ -0,0 +1,17 @@
+package Lab2;
+import java.util.Scanner;
+
+public class fruitprice {
+    public static void main(String[] args) {
+        Scanner kd = new Scanner(System.in);
+        int  fruit , basket , remain;
+        System.out.print("Input amount fruit : ");  
+        fruit = kd.nextInt();
+        basket = (int)fruit / 6;
+        remain = (int)fruit % 6;    
+        System.out.print("=== Calculate amount of fruit ===\n");
+        System.out.printf("Basket : " + basket + "\n");
+        System.out.printf("Remain fruit : " +  remain);
+    }
+    
+}
